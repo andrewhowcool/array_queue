fe8e493d9e024957cbf1e125402fb18515a895ff
Andrew Young <andrewcyoung1@gmail.com>
2017-03-09 20:34:40 +0800

    ok

--- array_queue/main.c
+++ array_queue/main.c
@@ -7,9 +7,102 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int queue[MAX];
+int front, rear;
+
+int isFull();
+int isEmpty();
+void add();
+void del();
+void display();
+
+int main(void)
+{
+    int choice;
+    
+    
+    while(1)
+    {
+        printf("enter option (-1 to quit)\n");
+        printf("(1) add \n(2)delete \n(3) display\n");
+        scanf("%d",&choice);
+        
+        if(choice==-1)
+            break;
+        
+        switch(choice)
+        {
+            case 1:		add();	break;
+            case 2:		del();	break;
+            case 3:		display();	break;
+            default:	printf("WRONG option !\n");	break;
+        }
+    }
+    
+    
     return 0;
 }
+
+
+//fuction
+int isFull()
+{
+    if((rear+1)%MAX == front)//if rear+1 ==front: full
+    {
+        printf("queue is full\n");
+        return 1;
+    }
+    return 0;
+}
+
+int isEmpty()
+{
+    if(front%MAX == rear%MAX)//if rear ==front: empty
+    {
+        printf("queue is empty\n");
+        return 1;
+    }
+    return 0;
+}
+
+void add()
+{
+    int data;
+    
+    if(isFull()==0)
+    {
+        printf("enter the data\n ");
+        scanf("%d",&data);
+        rear++;
+        rear%=MAX;//rear=rear%MAX
+        printf("enter data %d\n\n",data);
+        queue[rear] = data;
+    }
+}
+
+
+void del()
+{
+    if(isEmpty()==0)
+    {
+        front++;
+        front%=MAX;
+        printf("delete data %d\n\n",queue[front]);
+    }
+}
+
+void display()
+{
+    for(int i = (front+1)%MAX; i <= (rear%MAX);i++)
+    {
+        printf("%d ",queue[i]);
+        i%=MAX;
+    }
+    printf("\n");
+}
+
+
+
